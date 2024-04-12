21113c04b4966f9c4a1c621ef3166a5f498d1e3a
MohMohAung <mohmohaung737@gmail.com>
2024-04-13 01:53:46 +0630

    fix-background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import Photo from "../../public/gfs-temperature-at-1300-utc-on-march-11-2024.webp";
+import Photo1 from "../../public/istockphoto-1371476360-170667a.webp";
+import Image from "next/image";
 const page = () => {
   return (
-    <div className="w-full h-screen bg-green-900">
-      <div className="w-4/5 h-[80vh] m-auto flex flex-row justify-between items-center align-middle">
-        <div className="w-2/4 h-[100%] flex flex-col justify-center  items-center">
+    <div
+      className="w-full h-screen
+
+    inset-0 bg-green-900
+    "
+      style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 80%, 0% 70%)" }}
+    >
+      <div className="w-4/5 h-[70vh] m-auto flex flex-row justify-between items-center align-middle">
+        <div className="w-2/4 h-[100%] flex flex-col justify-center  items-center ">
           <h1 className="text-4xl text-white">Live Well</h1>
           <h1 className="text-4xl text-white">Eat well</h1>
           <button className="text-2xl w-40 p-1 border-2 border-white bg-white rounded-lg mt-5">
@@ -12,11 +20,16 @@ const page = () => {
           </button>
         </div>
         <div className="w-2/4 h-[100%] flex flex-col justify-center  items-center">
-          <h1 className="text-white text-2xl">World Photo</h1>
+          <Image src={Photo1} alt="" />
         </div>
+      </div>
+      <div>
+        <h1 className="text-red-900 mt-5">Care your healthy</h1>
       </div>
     </div>
   );
 };
 
 export default page;
+
+// bg-gradient-to-b from-green-900 via-green-900 to-white-500
